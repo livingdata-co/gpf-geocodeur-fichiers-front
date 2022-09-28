28bcb221dd487f44a1d5488fe87b6a69850a5f22
Marine Le Pennec <lepennec.marine@gmail.com>
2022-09-28 09:19:08 +0200

    Handle progressbar width with percent

--- components/progress-bar.js
+++ components/progress-bar.js
@@ -31,7 +31,7 @@ const ProgressBar = ({label, min, max}) => {
           position: absolute;
           top: 0;
           height: 100%;
-          width: 100%;
+          width: ${percent}%;
           background-color: ${colors.darkBlue};
         }
 
