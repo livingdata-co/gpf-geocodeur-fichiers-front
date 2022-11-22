182608c9c89e791af34f2ad71bb11da577867abd
Théophile Merlière <theophile.merliere@gmail.com>
2022-11-22 15:31:21 +0100

    Add link to home page on header

--- components/header.js
+++ components/header.js
@@ -1,21 +1,24 @@
 import PropTypes from 'prop-types'
+import Link from 'next/link'
 import Image from 'next/image'
 
 import theme from '@/styles/theme'
 
 const Header = ({isFrame, isMobile}) => (
   <header>
-    {isFrame || isMobile ? (
-      <div className='iframe-header'>
-        <Image src='/images/marianne.png' width={123} height={45} layout='fixed' />
-        <h1 className='iframe-title'>Géocodeur de fichiers</h1>
-      </div>
-    ) : (
-      <div className='header-container'>
-        <Image src='/images/repfr_logo.png' width={114} height={100} layout='fixed' />
-        <h1>Géocodeur de fichiers</h1>
-      </div>
-    )}
+    <Link href='/'>
+      {isFrame || isMobile ? (
+        <div className='iframe-header'>
+          <Image src='/images/marianne.png' width={123} height={45} layout='fixed' />
+          <h1 className='iframe-title'>Géocodeur de fichiers</h1>
+        </div>
+      ) : (
+        <div className='header-container'>
+          <Image src='/images/repfr_logo.png' width={114} height={100} layout='fixed' />
+          <h1>Géocodeur de fichiers</h1>
+        </div>
+      )}
+    </Link>
 
     <style jsx>{`
       header {
@@ -31,12 +34,14 @@ const Header = ({isFrame, isMobile}) => (
         gap: 2em;
         border-right: 1px solid ${theme.borderLight};
         padding: 1em;
+        cursor: pointer;
       }
 
       .iframe-header {
         display: flex;
         align-items: end;
         gap: 1em;
+        cursor: pointer;
       }
 
       .iframe-title {
