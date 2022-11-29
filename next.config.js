3abde48dfe923ca856912ba94b2c826946aade7d
Théophile Merlière <theophile.merliere@gmail.com>
2022-11-29 15:37:48 +0100

    Add express server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  useFileSystemPublicRoutes: false,
   swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true
---
+++ server.mjs
@@ -0,0 +1,38 @@
+import express from 'express'
+import next from 'next'
+import compression from 'compression'
+
+const port = process.env.PORT || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({dev})
+
+app.prepare().then(() => {
+  const server = express()
+
+  if (!dev) {
+    server.use(compression())
+  }
+
+  server.use(express.static('public')) // Serve favicon.ico
+
+  server.use(express.json())
+
+  server.get('/project/:projectId', (req, res) => {
+    app.render(req, res, '/project', {
+      ...req.query,
+      projectId: req.params.projectId
+    })
+  })
+
+  server.get('*', (req, res) => {
+    app.render(req, res, req.params[0], req.query)
+  })
+
+  server.listen(port, err => {
+    if (err) {
+      throw err
+    }
+
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
