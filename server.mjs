f26684daa2b26094943f19145af9bf8ce357ff4b
Théophile Merlière <theophile.merliere@gmail.com>
2022-11-29 16:02:37 +0100

    Use async/await

--- server.mjs
+++ server.mjs
@@ -5,27 +5,27 @@ const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
 
-app.prepare().then(() => {
-  const server = express()
+await app.prepare()
 
-  server.use(express.static('public')) // Serve favicon.ico
+const server = express()
 
-  server.get('/project/:projectId', (req, res) => {
-    app.render(req, res, '/project', {
-      ...req.query,
-      projectId: req.params.projectId
-    })
+server.use(express.static('public')) // Serve favicon.ico
+
+server.get('/project/:projectId', (req, res) => {
+  app.render(req, res, '/project', {
+    ...req.query,
+    projectId: req.params.projectId
   })
+})
 
-  server.get('*', (req, res) => {
-    app.render(req, res, req.params[0], req.query)
-  })
+server.get('*', (req, res) => {
+  app.render(req, res, req.params[0], req.query)
+})
 
-  server.listen(port, err => {
-    if (err) {
-      throw err
-    }
+server.listen(port, err => {
+  if (err) {
+    throw err
+  }
 
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+  console.log(`> Ready on http://localhost:${port}`)
 })
