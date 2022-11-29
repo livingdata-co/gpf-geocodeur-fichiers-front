82ba34e965190d27fdea41640a08fd9d036c9a99
Théophile Merlière <theophile.merliere@gmail.com>
2022-11-29 17:27:39 +0100

    Use js module config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   useFileSystemPublicRoutes: false,
@@ -8,4 +9,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+export default nextConfig
